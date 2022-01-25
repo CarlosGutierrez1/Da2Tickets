fb65d9705c034c9270222aed6323ef41ea71f151
KrLits <>
2022-01-24 19:06:19 -0500

    En proceso de maquetacion del index

---
+++ src/main/java/com/example/TicketsDemo/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.TicketsDemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+}
